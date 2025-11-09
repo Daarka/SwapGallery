7c3919fe1380926e563d1c915f8065a58d9aa899
Daarka <kasseldom@ymail.com>
2025-11-09 15:36:12 -0700

    x

--- fa.js
+++ fa.js
@@ -3,3 +3,14 @@ import '@awesome.me/webawesome/dist/styles/webawesome.css';
 
 // Optional: import your preferred theme
 import '@awesome.me/webawesome/dist/styles/themes/awesome.css';
+
+// https://webawesome.com/docs/components
+
+// <wa-icon>
+import '@awesome.me/webawesome/dist/components/icon/icon.js';
+
+// <wa-page>
+import '@awesome.me/webawesome/dist/components/page/page.js';
+
+// <wa-relative-time>
+import '@awesome.me/webawesome/dist/components/relative-time/relative-time.js';
